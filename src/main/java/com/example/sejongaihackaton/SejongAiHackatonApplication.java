c7b3a2eb2dc53c721a76c27348088233504391fa
GoToBILL <wnqudwnrlrl@naver.com>
2024-12-26 12:39:50 +0900

    feat: add settins

---
+++ src/main/java/com/example/sejongaihackaton/SejongAiHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.example.sejongaihackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongAiHackatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SejongAiHackatonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sejongaihackaton/SejongAiHackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sejongaihackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejongAiHackatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
