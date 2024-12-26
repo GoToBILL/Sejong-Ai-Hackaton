86718a9cdd19b896196fc480ccfa015126026e34
GoToBILL <wnqudwnrlrl@naver.com>
2024-12-26 14:18:47 +0900

    refactor: 설정 파일 수정

--- src/main/java/com/example/sejongaihackaton/SyncConfig.java
+++ src/main/java/com/example/sejongaihackaton/SyncConfig.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class SyncConfig {
-    @Value("${smartsync.api.key}")
     private static String apiKey;
+
+    @Value("${smartsync.api.key}")
+    public void setApiKey(String key) {
+        apiKey = key;
+    }
+
 }
