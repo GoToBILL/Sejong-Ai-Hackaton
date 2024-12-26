97c7f341c8f1b9a6408120fcde864f296da030d1
GoToBILL <wnqudwnrlrl@naver.com>
2024-12-26 13:37:43 +0900

    feat: setting file

---
+++ src/main/java/com/example/sejongaihackaton/SyncConfig.java
@@ -0,0 +1,12 @@
+package com.example.sejongaihackaton;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class SyncConfig {
+    @Value("${smartsync.api.key}")
+    private static String apiKey;
+}
